34f7aee3672467453d30411f3cfd51188fb2a19d
bitmunu <120228179+bitmunu@users.noreply.github.com>
2023-10-15 22:54:32 +0300

    meow

--- 0.py
+++ 0.py
@@ -9,7 +9,9 @@ for i in range(len(a)):
         if a[j] > a[i]:
             c[i].append(a[j])
 
-    if len(c[i]) > 0:
+    if len(c[i]) == 0:
+        print(f"Больше, чем {a[i]} элементов нет")
+    elif len(c[i]) > 0:
         print(f"Больше, чем {a[i]}: {', '.join(map(str, c[i]))}")
     else:
         c.pop()
